aee9fbd4cfd99e71818172c3cc84b8fc14f377dd
Will Lucena <will.lucen4@gmail.com>
2024-02-10 00:18:32 -0300

    feat: add new mock recipe

--- utils/mocks.ts
+++ utils/mocks.ts
@@ -1,6 +1,6 @@
 import { Recipe } from '@/models/recipe'
 
-export const mockResponse: Recipe = {
+const mockResponseA: Recipe = {
   imgUrl:
     'https://raw.githubusercontent.com/will-lucena/recipes-project/main/assets/images/image-omelette.jpeg',
   title: 'Simple Omelette Recipe',
@@ -38,17 +38,54 @@ export const mockResponse: Recipe = {
   slug: 'simple-omelette',
 }
 
+const mockResponseB: Recipe = {
+  imgUrl:
+    'https://raw.githubusercontent.com/will-lucena/recipes-project/main/assets/images/image-omelette.jpeg',
+  title: 'Mac and Cheese',
+  description:
+    'Mac and cheese—also called macaroni and cheese or mac n cheese in American English—is a dish of cooked macaroni pasta and a cheese sauce, most commonly cheddar. It can also incorporate other ingredients, such as breadcrumbs, meat and vegetables.',
+  preparationTime: {
+    total: 'Approximately 20 minutes',
+    preparation: '15 minutes',
+    cooking: '5 minutes',
+  },
+  ingredients: [
+    'Macaroni pasta',
+    'Cheese sauce (e.g. cheddar)',
+    'Breadcrumbs (optional)',
+    'Meat (optional)',
+    'Vegetables (optional)',
+  ],
+  instructions: [
+    'Boil the macaroni pasta in a large pot of salted water until al dente.',
+    'Drain the pasta and return it to the pot.',
+    'Add the cheese sauce to the pot and stir until the pasta is well coated.',
+    'If using, add the meat and vegetables to the pot and stir until well combined.',
+    'Transfer the mac and cheese to a baking dish and sprinkle with breadcrumbs.',
+    'Bake in a preheated oven at 180°C (350°F) for 20 minutes, or until the top is golden and the sauce is bubbling.',
+    'Serve hot.',
+  ],
+  nutrition: {
+    calories: 300,
+    fat: 20,
+    carbs: 30,
+    protein: 15,
+  },
+  slug: 'mac-and-cheese',
+}
+
 type RecipesDb = {
   [key: string]: Recipe
 }
 
 const mockDb: RecipesDb[] = [
   {
-    'simple-omelette': mockResponse,
+    'simple-omelette': mockResponseA,
   },
+  { 'mac-and-cheese': mockResponseB },
 ]
 
 export const mockDatabase = {
-  get: (slug: string) => mockDb.find((db) => db[slug])?.[slug] || null,
-  getAll: () => Object.values(mockDb).map((db) => Object.values(db)),
+  get: (slug: string) => mockDb.find((recipe) => recipe[slug])?.[slug],
+  getAll: () => mockDb.map((recipe) => Object.values(recipe)[0]),
 }
